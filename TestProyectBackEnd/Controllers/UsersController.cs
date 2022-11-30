7e819ed5e4a02772880686818a8573bbc74efd8f
Diego Torres <diegotorresx@gmail.com>
2022-11-30 07:18:05 -0500

    Implement DB and UserController

---
+++ TestProyectBackEnd/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using TestProyectBackEnd.Models;
+
+namespace TestProyectBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("registration")]
+        public Response register(Users user)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString(""));
+            return response;
+        }
+    }
+}
---
+++ TestProyectBackEnd/DataBase/DB.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using TestProyectBackEnd.Models;
+using System.Data;
+
+namespace TestProyectBackEnd.DataBase
+{
+    public class DB
+    {
+        public Response register(Users users,SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand command = new SqlCommand("sp_register",connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@FirstName", users.FirstName);
+            command.Parameters.AddWithValue("@LastName", users.LastName);
+            command.Parameters.AddWithValue("@Password", users.Password);
+            command.Parameters.AddWithValue("@Email", users.Email);
+            command.Parameters.AddWithValue("@Fund", 0);
+            command.Parameters.AddWithValue("@Type", "Users");
+            command.Parameters.AddWithValue("@Status", "Pending");
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i>0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "User registered successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User registration failed";
+            }
+            return response;
+        }
+    }
+}
