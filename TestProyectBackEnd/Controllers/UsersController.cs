ddc53e4c44cd1047b6223c35d5b0e0ef11ffd2a6
Diego Torres <diegotorresx@gmail.com>
2022-11-30 07:47:55 -0500

    Add functions to controller

--- TestProyectBackEnd/Controllers/UsersController.cs
+++ TestProyectBackEnd/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
+using TestProyectBackEnd.DataBase;
 using TestProyectBackEnd.Models;
 
 namespace TestProyectBackEnd.Controllers
@@ -19,7 +20,29 @@ namespace TestProyectBackEnd.Controllers
         public Response register(Users user)
         {
             Response response = new Response();
-            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString(""));
+            DB db = new DB();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Conn").ToString());
+            response = db.register(user,connection);
+            return response;
+        }
+        [HttpPost]
+        [Route("login")]
+        public Response login(Users user)
+        {
+            Response response = new Response();
+            DB db = new DB();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Conn").ToString());
+            response = db.Login(user, connection);
+            return response;
+        }
+        [HttpPost]
+        [Route("viewUser")]
+        public Response viewUser(Users user)
+        {
+            Response response = new Response();
+            DB db = new DB();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Conn").ToString());
+            response = db.viewUser(user, connection);
             return response;
         }
     }
--- TestProyectBackEnd/DataBase/DB.cs
+++ TestProyectBackEnd/DataBase/DB.cs
@@ -33,5 +33,65 @@ namespace TestProyectBackEnd.DataBase
             }
             return response;
         }
+        public Response Login(Users users, SqlConnection connection)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("sp_login",connection);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand.Parameters.AddWithValue("@Email",users.Email);
+            adapter.SelectCommand.Parameters.AddWithValue("@Password", users.Password);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count > 0)
+            {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                response.StatusCode=200;
+                response.StatusMessage = "User is valid";
+                response.user = user;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User is invalid";
+                response.user = null;
+            }
+            return response;
+        }
+        public Response viewUser(Users users, SqlConnection connection)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("sp_viewUser", connection);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand.Parameters.AddWithValue("@ID", users.ID);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count > 0)
+            {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                user.Fund = Convert.ToDecimal(dt.Rows[0]["Fund"]);
+                user.CreatedOn = Convert.ToDateTime(dt.Rows[0]["CreatedOn"]);
+                user.Password = Convert.ToString(dt.Rows[0]["Password"]);
+                response.StatusCode = 200;
+                response.StatusMessage = "User exists";
+                response.user = user;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User does not exists";
+                response.user = null;
+            }
+            return response;
+        }
     }
 }
