50d938df74a2c40178b9628e2e33995243063222
Diego Torres <diegotorresx@gmail.com>
2022-11-30 05:19:05 -0500

    First Commit

---
+++ TestProyectBackEnd/Models/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProyectBackEnd.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public List<Users> listUsers { get; set; }
+        public Users user { get; set; }
+    }
+}
